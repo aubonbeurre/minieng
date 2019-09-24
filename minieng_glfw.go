e8b1b154540123aea60664517340f479cb801d14
Alexandre Parenteau <aubonbeurre@gmail.com>
2019-09-23 22:00:01 -0700

    Add imgui

--- minieng_glfw.go
+++ minieng_glfw.go
@@ -5,6 +5,7 @@ package minieng
 import (
 	"fmt"
 	"io"
+	"math"
 	"os"
 	"os/signal"
 	"runtime"
@@ -13,7 +14,19 @@ import (
 
 	"github.com/aubonbeurre/glplus"
 	"github.com/go-gl/glfw3/v3.2/glfw"
+	"github.com/inkyblackness/imgui-go"
 )
+
+// GLFW implements a platform based on github.com/go-gl/glfw (v3.2).
+type GLFW struct {
+	imguiIO imgui.IO
+
+	window      *glfw.Window
+	imguirender *glplus.OpenGL3
+
+	time             float64
+	mouseJustPressed [3]bool
+}
 
 var (
 	// Backend ...
@@ -25,7 +38,9 @@ var (
 	// ResizeYOffset ...
 	ResizeYOffset = float32(0)
 
-	window *glfw.Window
+	window   *glfw.Window
+	context  *imgui.Context
+	platform *GLFW
 
 	canvasWidth  float32
 	canvasHeight float32
@@ -72,6 +87,8 @@ func keyCallback(w *glfw.Window, k glfw.Key, scancode int, a glfw.Action, mods g
 	} else if a == glfw.Release {
 		Input.keys.Set(key, false)
 	}
+
+	platform.keyChange(w, k, scancode, a, mods)
 }
 
 func mouseDownCallback(w *glfw.Window, b glfw.MouseButton, a glfw.Action, m glfw.ModifierKey) {
@@ -88,6 +105,8 @@ func mouseDownCallback(w *glfw.Window, b glfw.MouseButton, a glfw.Action, m glfw
 	} else {
 		Input.Mouse.Action = Release
 	}
+
+	platform.mouseButtonChange(w, b, a, m)
 }
 
 func mouseMoveCallback(w *glfw.Window, x float64, y float64) {
@@ -100,6 +119,8 @@ func mouseMoveCallback(w *glfw.Window, x float64, y float64) {
 func mouseWheelCallback(w *glfw.Window, xoff float64, yoff float64) {
 	Input.Mouse.ScrollX = float32(xoff)
 	Input.Mouse.ScrollY = float32(yoff)
+
+	platform.mouseScrollChange(w, xoff, yoff)
 }
 
 func onSizeCallback(w *glfw.Window, width int, height int) {
@@ -122,8 +143,134 @@ func onSizeCallback(w *glfw.Window, width int, height int) {
 	Mailbox.Dispatch(message)
 }
 
+func (p *GLFW) setKeyMapping() {
+	// Keyboard mapping. ImGui will use those indices to peek into the io.KeysDown[] array.
+	p.imguiIO.KeyMap(imgui.KeyTab, int(glfw.KeyTab))
+	p.imguiIO.KeyMap(imgui.KeyLeftArrow, int(glfw.KeyLeft))
+	p.imguiIO.KeyMap(imgui.KeyRightArrow, int(glfw.KeyRight))
+	p.imguiIO.KeyMap(imgui.KeyUpArrow, int(glfw.KeyUp))
+	p.imguiIO.KeyMap(imgui.KeyDownArrow, int(glfw.KeyDown))
+	p.imguiIO.KeyMap(imgui.KeyPageUp, int(glfw.KeyPageUp))
+	p.imguiIO.KeyMap(imgui.KeyPageDown, int(glfw.KeyPageDown))
+	p.imguiIO.KeyMap(imgui.KeyHome, int(glfw.KeyHome))
+	p.imguiIO.KeyMap(imgui.KeyEnd, int(glfw.KeyEnd))
+	p.imguiIO.KeyMap(imgui.KeyInsert, int(glfw.KeyInsert))
+	p.imguiIO.KeyMap(imgui.KeyDelete, int(glfw.KeyDelete))
+	p.imguiIO.KeyMap(imgui.KeyBackspace, int(glfw.KeyBackspace))
+	p.imguiIO.KeyMap(imgui.KeySpace, int(glfw.KeySpace))
+	p.imguiIO.KeyMap(imgui.KeyEnter, int(glfw.KeyEnter))
+	p.imguiIO.KeyMap(imgui.KeyEscape, int(glfw.KeyEscape))
+	p.imguiIO.KeyMap(imgui.KeyA, int(glfw.KeyA))
+	p.imguiIO.KeyMap(imgui.KeyC, int(glfw.KeyC))
+	p.imguiIO.KeyMap(imgui.KeyV, int(glfw.KeyV))
+	p.imguiIO.KeyMap(imgui.KeyX, int(glfw.KeyX))
+	p.imguiIO.KeyMap(imgui.KeyY, int(glfw.KeyY))
+	p.imguiIO.KeyMap(imgui.KeyZ, int(glfw.KeyZ))
+}
+
+var glfwButtonIndexByID = map[glfw.MouseButton]int{
+	glfw.MouseButton1: 0,
+	glfw.MouseButton2: 1,
+	glfw.MouseButton3: 2,
+}
+
+var glfwButtonIDByIndex = map[int]glfw.MouseButton{
+	0: glfw.MouseButton1,
+	1: glfw.MouseButton2,
+	2: glfw.MouseButton3,
+}
+
+func (p *GLFW) mouseButtonChange(window *glfw.Window, rawButton glfw.MouseButton, action glfw.Action, mods glfw.ModifierKey) {
+	buttonIndex, known := glfwButtonIndexByID[rawButton]
+
+	if known && (action == glfw.Press) {
+		p.mouseJustPressed[buttonIndex] = true
+	}
+}
+
+func (p *GLFW) mouseScrollChange(window *glfw.Window, x, y float64) {
+	p.imguiIO.AddMouseWheelDelta(float32(x), float32(y))
+}
+
+func (p *GLFW) keyChange(window *glfw.Window, key glfw.Key, scancode int, action glfw.Action, mods glfw.ModifierKey) {
+	if action == glfw.Press {
+		p.imguiIO.KeyPress(int(key))
+	}
+	if action == glfw.Release {
+		p.imguiIO.KeyRelease(int(key))
+	}
+
+	// Modifiers are not reliable across systems
+	p.imguiIO.KeyCtrl(int(glfw.KeyLeftControl), int(glfw.KeyRightControl))
+	p.imguiIO.KeyShift(int(glfw.KeyLeftShift), int(glfw.KeyRightShift))
+	p.imguiIO.KeyAlt(int(glfw.KeyLeftAlt), int(glfw.KeyRightAlt))
+	p.imguiIO.KeySuper(int(glfw.KeyLeftSuper), int(glfw.KeyRightSuper))
+}
+
+func (p *GLFW) charChange(window *glfw.Window, char rune) {
+	p.imguiIO.AddInputCharacters(string(char))
+}
+
+// DisplaySize returns the dimension of the display.
+func (p *GLFW) DisplaySize() [2]float32 {
+	w, h := p.window.GetSize()
+	return [2]float32{float32(w), float32(h)}
+}
+
+// FramebufferSize returns the dimension of the framebuffer.
+func (p *GLFW) FramebufferSize() [2]float32 {
+	w, h := p.window.GetFramebufferSize()
+	return [2]float32{float32(w), float32(h)}
+}
+
+// NewFrame marks the begin of a render pass. It forwards all current state to imgui IO.
+func (p *GLFW) NewFrame() {
+	// Setup display size (every frame to accommodate for window resizing)
+	if platform.imguirender == nil {
+		var err error
+		if platform.imguirender, err = glplus.NewOpenGL3(platform.imguiIO); err != nil {
+			panic(err)
+		}
+	}
+	displaySize := p.DisplaySize()
+	p.imguiIO.SetDisplaySize(imgui.Vec2{X: displaySize[0], Y: displaySize[1]})
+
+	// Setup time step
+	currentTime := glfw.GetTime()
+	if p.time > 0 {
+		p.imguiIO.SetDeltaTime(float32(currentTime - p.time))
+	}
+	p.time = currentTime
+
+	// Setup inputs
+	if p.window.GetAttrib(glfw.Focused) != 0 {
+		x, y := p.window.GetCursorPos()
+		p.imguiIO.SetMousePosition(imgui.Vec2{X: float32(x), Y: float32(y)})
+	} else {
+		p.imguiIO.SetMousePosition(imgui.Vec2{X: -math.MaxFloat32, Y: -math.MaxFloat32})
+	}
+
+	for i := 0; i < len(p.mouseJustPressed); i++ {
+		down := p.mouseJustPressed[i] || (p.window.GetMouseButton(glfwButtonIDByIndex[i]) == glfw.Press)
+		p.imguiIO.SetMouseButtonDown(i, down)
+		p.mouseJustPressed[i] = false
+	}
+}
+
+// ClipboardText returns the current clipboard text, if available.
+func (p *GLFW) ClipboardText() (string, error) {
+	return p.window.GetClipboardString()
+}
+
+// SetClipboardText sets the text as the current clipboard text.
+func (p *GLFW) SetClipboardText(text string) {
+	p.window.SetClipboardString(text)
+}
+
 // CreateWindow ...
 func CreateWindow(title string, width, height int) {
+	context = imgui.CreateContext(nil)
+
 	// make sure that we display any errors that are encountered
 	//glfw.SetErrorCallback(errorCallback)
 
@@ -183,11 +330,19 @@ func CreateWindow(title string, width, height int) {
 	canvasWidth = float32(x)
 	canvasHeight = float32(y)
 	retinaScale = canvasWidth / windowWidth
+
+	platform = &GLFW{
+		imguiIO: imgui.CurrentIO(),
+		window:  window,
+	}
+	platform.setKeyMapping()
+	window.SetCharCallback(platform.charChange)
 }
 
 // DestroyWindow ...
 func DestroyWindow() {
 	glfw.Terminate()
+	context.Destroy()
 }
 
 // RunPreparation is called automatically when calling Open. It should only be called once.
@@ -204,8 +359,17 @@ func RunIteration() {
 	Input.update()
 	glfw.PollEvents()
 
+	// Signal start of a new frame
+	platform.NewFrame()
+	imgui.NewFrame()
+
 	// Then update the world and all Systems
 	currentWorld.Update(Time.Delta())
+
+	// Rendering
+	imgui.Render() // This call only creates the draw data list. Actual rendering to framebuffer is done below.
+
+	platform.imguirender.Render(platform.DisplaySize(), platform.FramebufferSize(), imgui.RenderedDrawData())
 
 	// Lastly, forget keypresses and swap buffers
 	// reset values to avoid catching the same "signal" twice
